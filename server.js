b6e75fde511b4a2868c42387d09031774ed6a5eb
wistell <wistera.mono@gmail.com>
2024-08-20 01:06:19 +0900

    add

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT | 10000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
